09ef986205d1c591c0b32a7992f54dd8d7e65bb4
TreyPoynter <treypoynter@gmail.com>
2023-09-05 11:50:01 -0500

    Update muiltiplicationTable.mjs
    added error

--- Labs/Lab01-01/Multiplication_Table/muiltiplicationTable.mjs
+++ Labs/Lab01-01/Multiplication_Table/muiltiplicationTable.mjs
@@ -1,5 +1,5 @@
 import * as readline from 'node:readline/promises';
-import {stdin as input, stdout as output} from 'node:process';
+import {exit, stdin as input, stdout as output} from 'node:process';
 const rl = readline.createInterface({input, output});
 
 let tableSize = 0;
@@ -7,6 +7,11 @@ let tableSize = 0;
 do {
     tableSize = await rl.question("How big do you want the table to be (1-12) : ");
 } while (tableSize < 0 || tableSize > 12);
+
+if (isNaN(tableSize)) {
+    console.log("ERROR");
+    exit();
+}
 
 for (let r = 1; r <= tableSize; r++) {
     for (let c = 1; c <= tableSize; c++) {
